42d5dab7bcac05dc2a2f5219fabdfb19c5f6f530
Krzysztof Staszek <61710462+KStasz@users.noreply.github.com>
2024-11-29 11:39:08 +0100

    Validators added

---
+++ API/ShopShare/ShopShare.Application/Authentication/Query/LoginQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ShopShare.Application.Authentication.Query
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
