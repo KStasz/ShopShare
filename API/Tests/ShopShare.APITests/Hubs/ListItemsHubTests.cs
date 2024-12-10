547e520ed958c9f2c4ddfae65f77cfe2999c690c
Krzysztof Staszek <61710462+KStasz@users.noreply.github.com>
2024-12-10 09:27:46 +0100

    Moved project files to specific catalogs

---
+++ API/Tests/ShopShare.APITests/Hubs/ListItemsHubTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ShopShare.API.Hubs.Tests
+{
+    public class ListItemsHubTests
+    {
+        [Fact()]
+        public void CreateListItemTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
